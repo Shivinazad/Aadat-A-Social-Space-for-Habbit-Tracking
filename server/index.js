ef625183d211b6f46d685f12ceda83eb186eda97
sambhav-007 <sambhav.sehgal.007@gmail.com>
2025-11-28 14:13:04 +0530

    feat: Set up initial Express server with database connection, middleware, and API routes.

--- server/index.js
+++ server/index.js
@@ -115,7 +115,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client-react/dist')));
 
     // Handle React routing, return all requests to React app
-    app.get('*', (req, res) => {
+    app.get('(.*)', (req, res) => {
         res.sendFile(path.join(__dirname, '../client-react/dist', 'index.html'));
     });
 }
